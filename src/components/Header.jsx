1632835ec40d4c279b0f7209d02e5aad2ebaca18
zgronska <zgronska@gmail.com>
2023-01-17 14:43:47 +0100

    Styled the main section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,22 +5,24 @@ import Picture from "../pfp.jpg";
 export default function Header() {
   return (
     <header>
-      <img src={Picture} alt="" />
-      <h1 className="header--title">Zuza Grońska</h1>
-      <h2 className="header--subtitle">Frontend Developer</h2>
-      <div className="header--buttons">
-        <a
-          href="mailto:zgronska@gmail.com"
-          className="btn btn-header btn-email">
-          <FontAwesomeIcon icon="fa-solid fa-envelope" />
-          Email
-        </a>
-        <a
-          href="https://www.linkedin.com/in/zuza-gro%C5%84ska-2b2601116/"
-          className="btn btn-header btn-linkedin">
-          <FontAwesomeIcon icon="fa-brands fa-linkedin" />
-          LinkedIn
-        </a>
+      <img src={Picture} alt="" className="header--picture" />
+      <div className="header--content">
+        <h1 className="header--title">Zuza Grońska</h1>
+        <h2 className="header--subtitle">Frontend Developer</h2>
+        <div className="header--buttons">
+          <a
+            href="mailto:zgronska@gmail.com"
+            className="btn btn-header btn-email">
+            <FontAwesomeIcon icon="fa-solid fa-envelope" />
+            Email
+          </a>
+          <a
+            href="https://www.linkedin.com/in/zuza-gro%C5%84ska-2b2601116/"
+            className="btn btn-header btn-linkedin">
+            <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+            LinkedIn
+          </a>
+        </div>
       </div>
     </header>
   );
